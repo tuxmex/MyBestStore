eae504d6374000de31a692c296c460ecd81dfe2c
targ <crackiman@gmail.com>
2020-05-16 13:58:08 -0500

    Modifique la clase ProductTester para manejar varios productos mediante una matriz unidimensional si se introduce un valor superior a cero.

--- src/mx/store/ProductTest.java
+++ src/mx/store/ProductTest.java
@@ -1,5 +1,6 @@
 package mx.store;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ProductTest {
@@ -15,19 +16,35 @@ public class ProductTest {
         double tempPrice;
 
         //Int variable maxSize
-        int maxSize;
+        int maxSize = -1;
 
         //prompt the user until they enter a number >= zero
         do {
-            //Display prompt to user
-            System.out.println("Enter the number of products you would like to add.");
-            System.out.println("Enter 0 (zero) if you do not wish to add products: ");
-            //assume that the user enters a valid
-            maxSize = in.nextInt();
-            if (maxSize < 0)
-                System.out.println("Incorrect value entered");
-            //endif
+            try {
+                //Display prompt to user
+                System.out.println("Enter the number of products you would like to add.");
+                System.out.println("Enter 0 (zero) if you do not wish to add products: ");
+                //assume that the user enters a valid
+                maxSize = in.nextInt();
+                if (maxSize < 0)
+                    System.out.println("Incorrect value entered");
+                //endif
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect data type entered!");
+                in.nextLine();
+            } catch (Exception e) {
+                System.out.println(e);
+                in.nextLine();
+            }
         } while (maxSize < 0);
+
+        if (maxSize == 0) {
+            //Display a no products message if zero is entered
+            System.out.println("No products required!");
+        } else {
+            Product[] products = new Product[maxSize];
+        }//endif
+
 
         //Ask the user to enter the product information
         System.out.println("\n\nPlease enter the product name: ");
