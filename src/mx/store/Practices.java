f81675acbee0854feea9ff8864261fa6c1d10ee4
targ <crackiman@gmail.com>
2020-05-16 01:39:40 -0500

    Pida al usuario que introduzca valores para cada uno de los atributos de la clase Product. Solicite el nombre, la cantidad, el precio y el número de elemento, y almacene los valores en las variables locales temporales que acaba de crear.

---
+++ src/mx/store/Practices.java
@@ -0,0 +1,27 @@
+package mx.store;
+
+public class Practices {
+    private static int a;
+
+    public static void main(String[] args) {
+
+        /*String ocean = new String("Atlantic Ocean");
+        System.out.println(ocean.indexOf('a'));*/
+        /*int x;
+        int y = 4;
+        int z = 10;
+        x = z%y;
+        System.out.println(x);*/
+        /*int z=5, m=6;
+        double x = z/m;
+        System.out.println(x);*/
+       /* int num[]={9,8,7,6,5,4,3,2,1};
+        for(int i=0;i<9;i=i+3)
+            System.out.print(num[i]);*/
+        System.out.println(args.length);
+
+    }
+
+
+
+}
--- src/mx/store/ProductTest.java
+++ src/mx/store/ProductTest.java
@@ -1,24 +1,29 @@
 package mx.store;
 
+import java.util.Scanner;
+
 public class ProductTest {
     public static void main(String[] args) {
-        // Creating two objects with the default constructor
-        Product p1 = new Product();
-        Product p2 = new Product();
 
-        // Creating 4 products with values in arguments of
-        // constructor with parameters
+        //Create a Scanner object for keyboard input
+        Scanner in = new Scanner(System.in);
 
-        Product p3 = new Product(3, "Jumanji", 10, 5.55);
-        Product p4 = new Product(4, "Avengers", 20, 6.66);
-        Product p5 = new Product(5, "Dragon Ball Super Broly", 30, 7.77);
-        Product p6 = new Product(6, "La vida es Bella", 40, 8.88);
+        //Create local variables
+        int tempNumber;
+        String tempName;
+        int tempQty;
+        double tempPrice;
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println(p4);
-        System.out.println(p5);
-        System.out.println(p6);
+        //Ask the user to enter the product information
+        System.out.println("\n\nPlease enter the product name: ");
+        tempName = in.nextLine();
+        System.out.println("Please enter the quantity of the esto for this product: ");
+        tempQty = in.nextInt();
+        System.out.println("Please enter the price for this product: ");
+        tempPrice = in.nextDouble();
+        System.out.println("Please enter the item number: ");
+        tempNumber = in.nextInt();
+        
+
     }
 }
