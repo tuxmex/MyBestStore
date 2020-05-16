5478b3082483d36d9372c14b1a01daad79b8fdb8
targ <crackiman@gmail.com>
2020-05-16 14:16:53 -0500

    Elimine el código innecesario que no se utilice en este ejercicio y guarde el proyecto.

--- src/mx/store/ProductTest.java
+++ src/mx/store/ProductTest.java
@@ -64,41 +64,6 @@ public class ProductTest {
             }
         }//endif
 
-
-
-
-
-        //Ask the user to enter the product information
-        in.nextLine();
-
-        System.out.println("\n\nPlease enter the product name: ");
-        tempName = in.nextLine();
-        System.out.println("Please enter the quantity of the esto for this product: ");
-        tempQty = in.nextInt();
-        System.out.println("Please enter the price for this product: ");
-        tempPrice = in.nextDouble();
-        System.out.println("Please enter the item number: ");
-        tempNumber = in.nextInt();
-
-        //Create the second product using information input by the user
-        Product p2 = new Product(tempNumber, tempName, tempQty, tempPrice);
-
-        Product p3 = new Product(3, "Mi producto 3", 100, 55.5);
-        Product p4 = new Product(4, "Mi producto 4", 101, 66.5);
-        Product p5 = new Product(5, "Mi producto 5", 102, 77.5);
-        Product p6 = new Product(6, "Mi producto 6", 103, 88.5);
-
-
-        //Set the active field to false
-        p6.setActive(false);
-
-        //System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println(p4);
-        System.out.println(p5);
-        System.out.println(p6);
-
         //Close the scanner object
         in.close();
 
